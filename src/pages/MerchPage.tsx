4051d6e144f576d764695fd7423a54c23539cc7b
bjstudiosgit <bjstudiosspam@gmail.com>
2026-08-06 00:31:49 +0100

    Delete src directory

--- src/pages/MerchPage.tsx
+++
@@ -1,9 +0,0 @@
-import MerchSection from "../components/MerchSection";
-
-export default function MerchPage() {
-  return (
-    <main className="min-h-screen pt-28 md:pt-32">
-      <MerchSection />
-    </main>
-  );
-}
